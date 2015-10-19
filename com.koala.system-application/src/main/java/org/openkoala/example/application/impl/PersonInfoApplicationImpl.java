f51eef25347dac5908cd06a1673eb9a89696a01b
徐刘根 <1573876303@qq.com>
2015-10-19 22:54:07 +0800

    修改数据源

--- com.koala.system-application/src/main/java/org/openkoala/example/application/impl/PersonInfoApplicationImpl.java
+++ com.koala.system-application/src/main/java/org/openkoala/example/application/impl/PersonInfoApplicationImpl.java
@@ -12,33 +12,33 @@ import org.openkoala.example.domain.PersonInfo;
 @Named
 @Transactional
 public class PersonInfoApplicationImpl implements PersonInfoApplication {
-	
-	public PersonInfo getPersonInfo(Long id) {
-		return PersonInfo.get(PersonInfo.class, id);
-	}
-	
-	public void updatePersonInfo(PersonInfo personInfo) {
-		personInfo.save();
-	}
-	
-	public void savePersonInfo(PersonInfo personInfo) {
-		if(personInfo != null) {
-			personInfo.save();
-		}
-	}
 
-	public void removePersonInfo(PersonInfo personInfo) {
-		personInfo.remove();
-	}
+    public PersonInfo getPersonInfo(Long id) {
+        return PersonInfo.get(PersonInfo.class, id);
+    }
 
-	public void removePersonInfos(List<PersonInfo> personInfos) {
-		for(PersonInfo personInfo : personInfos){
-			personInfo.remove();
-		}
-	}
+    public void updatePersonInfo(PersonInfo personInfo) {
+        personInfo.save();
+    }
 
-	public List<PersonInfo> findAllPersonInfo() {
-		return PersonInfo.findAll(PersonInfo.class);
-	}
-	
+    public void savePersonInfo(PersonInfo personInfo) {
+        if (personInfo != null) {
+            personInfo.save();
+        }
+    }
+
+    public void removePersonInfo(PersonInfo personInfo) {
+        personInfo.remove();
+    }
+
+    public void removePersonInfos(List<PersonInfo> personInfos) {
+        for (PersonInfo personInfo : personInfos) {
+            personInfo.remove();
+        }
+    }
+
+    public List<PersonInfo> findAllPersonInfo() {
+        return PersonInfo.findAll(PersonInfo.class);
+    }
+
 }
